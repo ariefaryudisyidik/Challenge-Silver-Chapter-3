e8076690c80ea225987acf72b44443f8997e35f8
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-03-25 21:01:11 +0700

    Publish

---
+++ app/src/main/java/com/binar/ariefaryudisyidik/challengesilverchapter3/SecondFragment.kt
@@ -0,0 +1,46 @@
+package com.binar.ariefaryudisyidik.challengesilverchapter3
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.binar.ariefaryudisyidik.challengesilverchapter3.databinding.FragmentSecondBinding
+import com.google.android.material.textfield.TextInputEditText
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSecondBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGoToScreenThree.setOnClickListener {
+            val inputName = binding.edtName.text.toString().trim()
+
+            if (TextUtils.isEmpty(inputName)) {
+                Toast.makeText(activity, "Field tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            } else {
+                val person = Person(name = inputName)
+                findNavController().navigate(
+                    SecondFragmentDirections.actionSecondFragmentToThirdFragment(
+                        person
+                    )
+                )
+            }
+        }
+    }
+
+}
